f6a4fae2d666803f61371e2c4f7de9a2532b1d75
marcdledda <marcdl636@gmail.com>
2018-01-31 11:14:13 -0600

    fiddling

--- cashToCoins.js
+++ cashToCoins.js
@@ -1,1 +1,14 @@
 console.log("JS TEST");
+
+let dollarAmount = 0.35;
+let piggyBank = ["Quarters: ", 0, "Dimes: ", 0, "Nickels: ", 0, "Pennies: ", 0];
+
+function calculator(amount){
+    if ((amount / 0.25) > 0.9) {
+        piggyBank[1] = Math.floor(dollarAmount / 0.25);
+    } else if ((amount / 0.1) > 0.9) {
+        piggyBank[3] = Math.floor(dollarAmount / 0.1)
+    }
+}
+
+console.log(piggyBank);
